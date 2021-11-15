7859c1eb5eb6037aebe0e736678325333102ac42
Sharmin Peu <sharminpeu574@gmail.com>
2021-11-14 22:17:31 -0800

    added

--- src/App.test.js
+++
@@ -1,9 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import APP from './APP';
-// import App from './App';
-
-test('renders learn react link', () => {
-  render(<APP />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
